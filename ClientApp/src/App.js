a0b76514e5502b5cb2e520cbaa0a9ba3271446d8
angelDavid24 <angeldavidnavatrujillo@gmail.com>
2023-12-01 20:44:27 -0600

    Add files via upload

---
+++ ClientApp/src/App.js
@@ -0,0 +1,116 @@
+import { useEffect, useState } from "react"
+import { Col, Container, Row, Card, CardHeader, CardBody, Button } from "reactstrap"
+import ModalContacto from "./components/ModalContacto"
+import TablaContacto from "./components/TablaContacto"
+
+const App = () => {
+
+    const [contactos, setContactos] = useState([])
+    const [mostrarModal, setMostrarModal] = useState(false)
+    const [editar, setEditar] = useState(null)
+
+    const mostrarContactos = async () => {
+
+        const response = await fetch("api/contacto/Lista");
+
+        if (response.ok) {
+            const data = await response.json();
+            setContactos(data)
+        } else {
+            console.log("Error en los datos de la lista")
+        }
+
+    }
+
+    useEffect(() => {
+        mostrarContactos()
+    }, [])
+
+    const guardarContacto = async (contacto) => {
+
+        const response = await fetch("api/contacto/Guardar", {
+
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(contacto)
+        })
+
+        if (response.ok) {
+            setMostrarModal(!mostrarModal);
+            mostrarContactos();
+        }
+
+    }
+
+    const editarContacto = async (contacto) => {
+
+        const response = await fetch("api/contacto/Editar", {
+
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(contacto)
+        })
+
+        if (response.ok) {
+            setMostrarModal(!mostrarModal);
+            mostrarContactos();
+        }
+
+    }
+
+    const eliminarContacto = async (id) => {
+
+        var respuesta = window.confirm("Desea eliminar el contacto?")
+
+        if (!respuesta) {
+            return;
+        }
+
+        const response = await fetch("api/contacto/Eliminar/" + id, {
+            method: 'DELETE'
+        })
+
+        if (response.ok) {
+            mostrarContactos();
+        }
+
+    }
+
+    return (
+        <Container>
+            <Row className="mt-5">
+                <Col sm="12">
+                    <Card>
+                        <CardHeader>
+                        <h5>Lista de Contactos</h5>
+                        </CardHeader>
+                        <CardBody>
+                            <Button size="sm" color="success" onClick={() => setMostrarModal(!mostrarModal)}>Nuevo Contacto</Button>
+                            <hr></hr>
+                            <TablaContacto data={contactos}
+                                setEditar={setEditar}
+                                mostrarModal={mostrarModal}
+                                setMostrarModal={setMostrarModal}
+                                eliminarContacto={eliminarContacto}
+                            />
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+            <ModalContacto
+                mostrarModal={mostrarModal}
+                setMostrarModal={setMostrarModal}
+                guardarContacto={guardarContacto}
+                editar={editar}
+                setEditar={setEditar}
+                editarContacto={editarContacto}
+            />
+        </Container>
+    )
+}
+
+export default App;
---
+++ Controllers/ContactoController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoCRUDMVC.Models;
+
+namespace ProyectoCRUDMVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactoController : ControllerBase
+    {
+        private readonly CrudreactmvcContext _dbcontext;
+
+        public ContactoController(CrudreactmvcContext context)
+        {
+            _dbcontext = context;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> Lista()
+        {
+            List<Contacto> lista = await _dbcontext.Contactos.OrderByDescending(c => c.IdContacto).ToListAsync();
+
+            return StatusCode(StatusCodes.Status200OK, lista);
+        }
+
+        [HttpPost]
+        [Route("Guardar")]
+        public async Task<IActionResult> Guardar([FromBody] Contacto request)
+        {
+            await _dbcontext.Contactos.AddAsync(request);
+            await _dbcontext.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status200OK, "ok");
+        }
+
+        [HttpPut]
+        [Route("Editar")]
+        public async Task<IActionResult> Editar([FromBody] Contacto request)
+        {
+            _dbcontext.Contactos.Update(request);
+            await _dbcontext.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status200OK, "ok");
+        }
+
+        [HttpDelete]
+        [Route("Eliminar/{id:int}")]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            Contacto contacto = _dbcontext.Contactos.Find(id);
+
+            _dbcontext.Contactos.Remove(contacto);
+            await _dbcontext.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status200OK, "ok");
+        }
+
+    }
+}
